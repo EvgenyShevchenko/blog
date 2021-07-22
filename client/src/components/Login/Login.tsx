ebbe3116b2ca7b3ad82e6de5ca49d5974b98ec2f
Dev <dev@iMac-WiliXTeam.local>
2021-07-22 18:23:58 +0300

    add login form

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,7 +1,11 @@
-
-const Login : React.FC = () => {
-    return(
-        <div className="Login__title">Sign in</div>
+const Login: React.FC = () => {
+    return (
+        <form className="Login__form">
+            <h2 className="Login__title">Sign in</h2>
+            <input className='Login__input' name='email' type='text' placeholder='Enter your Email...'/>
+            <input className='Login__input' name='password' type='password' placeholder='Enter your Password...'/>
+            <button className='Login__button' type='submit'>Login</button>
+        </form>
 
     )
 }
